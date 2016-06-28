4563a4e7a756394f8bd9d7b98e894cbda1651c12
Forrest Oliphant <github.com@sembiki.com>
2016-06-28 00:09:05 -0400

    wip
    http://sketch.paperjs.org/#S/fVRNj5swEP0rIy4LLUHJqqtKUfe06rFVpa3aQ7IHx54UC2NT22S3ivjvtQEHTNpwQPD83ny8GTgnktSYbJPnCi0tkzyhivn3E9FwUK1kBh5hs95LD9iS00qimWNaNQiPewnu2oHEV/imuLTpJod1NsD5DF7nsPkHvPo/fRX4L0NC2bJf6CvYOWAvj62klisJJ3z7rp6U0syk7jmD8yDzGtpqjdI60RT6/sEFd7dsRuvVjuX0RU2aNAT3AVFkY0R/hYjvezIKeDe5M7E02lbLQB7wbkwYzvqce9lFzWgimaq/+lYhFSijbrSr8AuxZTGw0lkHp+GRHyHV8AnWxf3DRdpzIgtmpneAwuBMeEs3G+KV7uMt4eo644J7PfZu6o6Hxo/CuZZGHrgBeJ8ib89cMnzbAs/dkKhVegunbmk1Nz+I4Gz0ehxVPqxZZLuitG04snH1AlyqGpfuhB7HaMVR6c+Elpd9gvQUuTRtRlwbaRrx53ZlS0lNKny2xDqF/zRHulnyfeWN829Y9suH4yVZOGeavIbOHDX1fHfo4YIKZbBHA2KsVhU+KaH8ft4dBKHVXXT2k7M+4fQbWcGHWE0arzW/W6LRid3/6KCRVI031iTb3Uv3Fw==

--- labyrinth-01.paper.js
+++ labyrinth-01.paper.js
@@ -1,13 +1,15 @@
 // WIP
-// http://sketch.paperjs.org/#S/dVLLboMwEPyVFZdACxGp2kuknPIDlVqph5CDY7YCQdZ0bZJKEf/eNcQlkdo9WPbszOxDvkSkjhito7cGna6iNNKm9O+CToqBTYewKQgkdkB4hldTk4tXKeTJBKc3cJ7C6g84+5+eBf5+KuiqWjeE1sIGVnlBBX32pF1tCE74/W62xnBpY7kncJmUXqZ7ZiQnotn96UX85UhuaKNaWKJfHlUXB3NviG1ydfQRHB9HMrbwMPc2sxhdzxTIEz5cC4bcWLOgwQ/jm+iUq6YWfsfxa05CvmR1DoMINfZ8SXp4qVtjcUQDYh2bBremNSyixaFVulnc5T7qciw4rzaD53u16rzWfvWKUcTyCw6Mqun8Hm203u2HHw==
+// http://sketch.paperjs.org/#S/fVRNj5swEP0rIy4LLUHJqqtKUfe06rFVpa3aQ7IHx54UC2NT22S3ivjvtQEHTNpwQPD83ny8GTgnktSYbJPnCi0tkzyhivn3E9FwUK1kBh5hs95LD9iS00qimWNaNQiPewnu2oHEV/imuLTpJod1NsD5DF7nsPkHvPo/fRX4L0NC2bJf6CvYOWAvj62klisJJ3z7rp6U0syk7jmD8yDzGtpqjdI60RT6/sEFd7dsRuvVjuX0RU2aNAT3AVFkY0R/hYjvezIKeDe5M7E02lbLQB7wbkwYzvqce9lFzWgimaq/+lYhFSijbrSr8AuxZTGw0lkHp+GRHyHV8AnWxf3DRdpzIgtmpneAwuBMeEs3G+KV7uMt4eo644J7PfZu6o6Hxo/CuZZGHrgBeJ8ib89cMnzbAs/dkKhVegunbmk1Nz+I4Gz0ehxVPqxZZLuitG04snH1AlyqGpfuhB7HaMVR6c+Elpd9gvQUuTRtRlwbaRrx53ZlS0lNKny2xDqF/zRHulnyfeWN829Y9suH4yVZOGeavIbOHDX1fHfo4YIKZbBHA2KsVhU+KaH8ft4dBKHVXXT2k7M+4fQbWcGHWE0arzW/W6LRid3/6KCRVI031iTb3Uv3Fw==
 
+var bounds = 10
+var thickness = 10
 var rope =
     [ new Point(1, 0)
     , new Point(0, 1)
     , new Point(-1, 0)
     , new Point(0, -1)
     ]
-var thickness = 10
+var nudges = []
 
 function vexToCoords(vex) {
     var current = new Point(250, 250)
@@ -16,6 +18,38 @@ function vexToCoords(vex) {
         return current
     })
     return coords
+}
+
+function randomNudge (len) {
+    var r = Math.random()
+    var v
+    if (r < 0.25) {
+        v = new Point(1, 0)
+    } else if (r < 0.5) {
+        v = new Point(0, 1)
+    } else if (r < 0.75) {
+        v = new Point(-1, 0)
+    } else {
+        v = Point(0, -1)
+    }
+    var i = Math.floor(Math.random() * len)
+    return {index: i, vector: v}
+}
+
+function isValidNudge (current, nudge) {
+    var occupied = []
+    var home = new Point(0, 0)
+    current.forEach(function (v) {
+        
+    })
+}
+
+function applyNudge (current, nudge) {
+    
+}
+
+function makeState (rope, nudges) {
+    
 }
 
 var path = vexToCoords(rope)
