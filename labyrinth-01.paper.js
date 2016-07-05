71417e895f22bbfc0dd11bcaa199b867869bb040
Forrest Oliphant <github.com@sembiki.com>
2016-07-05 14:35:36 +0300

    fix symmetry (still bumps from center)

--- labyrinth-01.paper.js
+++ labyrinth-01.paper.js
@@ -1,19 +1,17 @@
 // WIP
-// http://sketch.paperjs.org/#S/rVffb9owEP5XLKapSQkssHaTWOlLtT1M2jSpk/YACKWJgYhgIycBupb/fedfiZ2YtuuWlxDf3Xffnf3Z5qFDog3ujDq3a1zEq07QiWnCv3cRQ8UqjdcE5zkao8GHKeFjecHoGv9Kk2IFo7VHD2mH9DcGy0Wo/SNWwDfBe/SDpqTwuMO7YYDkuzfwleP9ZoMLds9RWYmnRA5n0d09gzDINiUInokBNQhQ6MvhwBgOAzR4+XDvBMqpcYDpnYB3jhswM17VlCxKEhcpJWiHDz/pDaUsyT347aMH6ccLj0vGMOGt82QPz+tuK7hYRILHRDnPVDg+9BeUfY7iladzcXyc+SqBiFYJupBBGB0Z6iz9bZmvPBWjrEf1ZrgoGVGOU3K0amQRSejme5ksMfKsChkw/xYVq7508RRaukAeQ1co7A8vK38jjbOzx2bsM6HGWmiFfnwmttfO68IXi8HuBcFL5BHeiiqDjhT9OYegZgxIQjWvkkIg3QOUksRYNeILeirfb2vp9DNMlsWqrlNmG3NVLmvYiQic+a7qF1GWY6tkPoNzQ51GujxLY6ync26Mb4VBpIEWqjJ8m700d9/7UMHcXcKTkFCRhPV5GzVTGsflNsUJF8vMJTKhsX6cUVIxBwmBMsArBTtAAwX40eL0CaVX8Ate3a7VOh4qhWVGTdKZU4PCs7bwaVLW/j26AgLo8RHVI9di/7TyuafLmLIm7qGFe3g1rtnkuZ7Lmu65PBe6dSabkg6d2BizvydyCqk6V9qyrSenKb5ou81eKr+WILRyT0bWgqzjXOIz+bXZN8lzHgmL9vrQhR3WkwEPKMfLDXQ/H1lHT51cn17ylL+hGWUjdHaXRfH6zLZ9hW0OTIyWJGmYxOVgZH5UtCEBp8Z1lmPBzNeU91G2/n+U32CM/4Ux3GcuTNacnYu1XumhfYmZL6I0k8PGaloyujfOQHvHA20MfOeuPS04tNqzzfNU0Ches+6mhWPVTQu9HHUZFVVpEpkq1uLoXmQwH15rT3w39KEepQ/nPi4RK1R3AXOzgmpjDxQJ3+Qm6jkhI/2YOeZNUUmYI8Kwsdhl88ea2G63aTabJuf+Gl2God8GMsGAhrGP1T5Hi5HulU3NWZNRkhF8bO5qlHxhcOv38I7f5TRifdyNQ36mDcP2kcZXsGdffcSyVxpF4ydFKqTP8Ibu8K2K8ExTlCTVuIZUdqE/d6gwnQg9wh+aO4aj9ZbfyvLOaDI7/gE=
+// http://sketch.paperjs.org/#S/rVdtb9MwEP4rVhFasrQlHRugsu4Lgg9IIKQh8aGpqixx26ipXTnpG1v/O+e3xE7cbQzyJY3v7rnnzndn975D4hXuDDu3S1wmi063k9CUf29jhspFliwJLgo0QoN3EeFrRcnoEv/K0nIBq7VGgC6VQvYbg+Qy1PoxK+Gb4B36QTNSelzhzUUXybev1A6rFS7ZgWOyDY6IXM7juwMDI/AVEQTP2AAadFHoy+WusRx20cCx3DuhfmodYHouHEN9wmlGZLYhSZlRgrZ4/5N+opSlhQe/fXQv9XgkyYYxTHgmPJmS8zp5Ci4RlqAxVsoTZY73/Rlln+Nk4WlfHB/nvnIgrJWDADwIocND7aW/3hQLT9ko6VG9GS43jCjFiBytGFlMUrr6vknnGHlWhAyYf4vLRV+qeAotmyGPoWsU9i+uKn3DjTOzx6btE6bGnrdM3z9h22v7deGLYrBzQfAceYSnovKgLUV+zsGoaQM1rpJX1XZXqndRRlKjasQX5FS+X9e90M8xmZeLOk7pbTSqVcbCaOIKfBbnBW6lqoKAmLwWzPNweBFMjY41GBd5lmBdEVNjfS0Ewg3sgsqEbydAioO3PiRh6s7Co5AQkYT1+U5opjRJNusMp7zfJq4+FW3aT3JKKubQhdBcoJWBHKCBAvxocfqIsmv4Ba8gsFLHTWVvmlbjbOJsY6FZS/g2KWn/gK6BAHp4QPXKjZiolj/3dhlb1sTdt3D3L8Y1kzzVe1nTPZcnRVB7silp07GNMfl7IqeQqrPG0Fdg9eY0+zder/PndnCrIXTzn7SsG7K2czWfya/Nvkme80hZvNPHMAxpTxrcowLPV5D9YmidXrVzfQDKc/8TzSkborO7PE6WZ7bsK0xKEDG6IWlDJK4LQ/Ojog0OODXeZwUWzHxNeRfny/9H+RXG+F8Yox76YLLm7FysdaWH9sVmOouzXC4b1TRndGcco/bEg94Y+M7BH5UcWs1s80gWNMqX1F1UOqouKnU56jAqqlIkPFWsPXH8z3LYEK81FOGqBwGpBnEMci4daJcVtjuMqRlHNd67iopvMhRRnWgm/Zg+ps3WkjBHhGG82MHzx9reIGiKzdTJCrhBV2Hot4FMMKBhTLNa52gx0rmyqTljMkIyjI/N2UbJFwb/Bjy85ZdCjVgfeqOQn2wXYftg43Xs2XcoUfyqU9Ho0VYVA4DhFd3iW2XhmaI4Tat1DankogvdpkJ0wvQIf3TuGI6Xa369KzrD8eT4Bw==
 
 var thickness = 16
-var strokeWidth = thickness - 6
+var strokeWidth = thickness + 4
 var size = 40
-var start = new Point(size/2, size/2-1)
+var start = new Point(size/2, size/2)
 var symmetry = true
 
 var labyrinth =
     [ new Point(1, 0)
     , new Point(0, 1)
-    , new Point(0, 1)
     , new Point(-1, 0)
     , new Point(-1, 0)
-    , new Point(0, -1)
     , new Point(0, -1)
     , new Point(1, 0)
     ]
@@ -49,6 +47,9 @@ function neg (nudge) {
 
 function tryNudge (labyrinth, nudge, index) {
     index = index % labyrinth.length
+    if (nudge == labyrinth[index]) {
+        return false
+    }
     if (nudge == neg(labyrinth[index])) {
         return false
     }
@@ -98,7 +99,7 @@ var walk = new Path(
     { segments: vexToCoords(labyrinth)
     , strokeColor: '#eee'
     , strokeJoin: 'round'
-    , strokeWidth: strokeWidth - 4
+    , strokeWidth: strokeWidth - 8
     }
 )
 walk.closePath()
@@ -112,7 +113,7 @@ function grow () {
 	var _labyrinth = tryNudge(labyrinth, nudge, index)
 	if (_labyrinth) {
 	    if (symmetry) {
-    	    var _index = (Math.floor(_labyrinth.length/2) + index) % _labyrinth.length
+    	    var _index = ((Math.floor(_labyrinth.length/2) + index) % _labyrinth.length) + 1
 	        var __labyrinth = tryNudge(_labyrinth, neg(nudge), _index)
     	    if (__labyrinth) {
                 labyrinth = __labyrinth
