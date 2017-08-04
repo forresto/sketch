b02189b8b96dd705d8bc2766dd054028c16fbdf8
Forrest Oliphant <forrest@sembiki.com>
2017-08-05 02:48:34 +0300

    generate mutations, custom expand
    MakerJs.model.expandPaths got crazy slow

--- labyrinth-03.maker.ts
+++ labyrinth-03.maker.ts
@@ -1,37 +1,30 @@
 /// <reference path="node_modules/makerjs/index.d.ts" />
 
-import * as MakerJs from 'makerjs';
-
 /*
 
-← ← ↑ → → ↑ → ↑ ↑ → ↓ ↓ → → ↓ ← ← ↓ ← ↓ ↓ ← ↑ ↑
+↑ ↑ ← ↓ ↓ ↓ ↓ ↓ ← ↑ ↑ ↑ ← ↓ ↓ ↓ ← ↑ ↑ ↑ ← ↓ ↓ ↓ ← ↑ ↑ ↑ ↑ → ↑ ← ↑ → ↑ ↑ ← ↑ → → ↓ ↓ ↓ ↓ ↓ → → ↑ ← ↑ → ↑ → ↓ → ↑ ↑ ← ← ← ↑ ↑ ← ← ← ↑ → → → → ↓ ↓ → ↑ ↑ → ↓ ↓ → ↑ → ↑ → ↓ → → ↑ → ↓ → ↑ → ↓ ↓ ← ← ← ← ← ← ↓ ← ↓ → ↓ ← ↓ → ↓ ↓ → ↑ ↑ ↑ ↑ ↑ → ↓ ↓ ↓ → ↑ ↑ ↑ → ↓ ↓ ↓ → ↑ ↑ ↑ → ↓ ↓ ↓ ↓ ← ↓ → ↓ ← ↓ ↓ → ↓ ← ← ↑ ↑ ↑ ↑ ↑ ← ← ↓ → ↓ ← ↓ ← ↑ ← ↓ ↓ → → → ↓ ↓ → → → ↓ ← ← ← ← ↑ ↑ ← ↓ ↓ ← ↑ ↑ ← ↓ ← ↓ ← ↑ ← ← ↓ ← ↑ ← ↓ ← ↑ ↑ → → → → → → ↑ → ↑ ← ↑ → ↑ ←
 
-. . . → ↓ . . 
-. . . ↑ ↓ . . 
-. . → ↑ → → ↓ 
-→ → ↑ . ↓ ← ← 
-↑ ← ← ↓ ← . . 
-. . ↑ ↓ . . . 
-. . ↑ ← . . . 
+→ → → → ↓ → ↓ . → ↓ . → ↓ → ↓ 
+↑ ← ← ← ↓ ↑ ↓ → ↑ → → ↑ → ↑ ↓ 
+→ → ↓ ↑ → ↑ → ↑ ↓ ← ← ← ← ← ← 
+↑ ← ↓ ↑ ← ← ← ↓ ← → ↓ → ↓ → ↓ 
+. ↑ ↓ . → ↓ ↑ → ↓ ↑ ↓ ↑ ↓ ↑ ↓ 
+→ ↑ ↓ → ↑ → ↑ ↓ ← ↑ ↓ ↑ ↓ ↑ ↓ 
+↑ ← ↓ ↑ ← ↓ ← → ↓ ↑ → ↑ → ↑ ↓ 
+→ ↑ → → ↑ ↓ ↑ . ↓ ↑ ↓ ← ← ↓ ← 
+↑ ↓ ← ↓ ← ↓ ↑ ← → ↑ → ↓ ↑ → ↓ 
+↑ ↓ ↑ ↓ ↑ ↓ → ↑ ↓ ← ↓ ← ↑ ↓ ← 
+↑ ↓ ↑ ↓ ↑ ↓ ↑ ← ↓ ↑ ← . ↑ ↓ . 
+↑ ← ↑ ← ↑ ← → ↑ → → → ↓ ↑ → ↓ 
+→ → → → → → ↑ ↓ ← ↓ ← ↓ ↑ ← ← 
+↑ ↓ ← ↓ ← ← ↓ ← ↑ ↓ ↑ → → → ↓ 
+↑ ← ↑ ← . ↑ ← . ↑ ← ↑ ← ← ← ← 
 
 */
 
-const SIZE = 7;
-const MAX = Math.floor(SIZE / 2);
-const MIN = -1 * MAX;
-const SCALE = 72;
-const RADIUS = 24;
-const STROKE = 12;
-const symmetry = true;
-const DEBUG = true;
+import * as MakerJs from 'makerjs';
 
 type Point = [number, number];
-
-const LEFT: Point = [-1, 0];
-const RIGHT: Point = [1, 0];
-const UP: Point = [0, -1];
-const DOWN: Point = [0, 1];
-
 const enum Direction {
   Up,
   Down,
@@ -40,6 +33,35 @@ const enum Direction {
 }
 
 type Path = Array<Direction>;
+type AbsPath = Array<Point>;
+
+type BoardSpace = {
+  direction: Direction;
+  point: Point;
+  index: number;
+  next: number;
+} | null;
+type Board = Array<BoardSpace>;
+
+type Mutation = [number, Direction];
+type Mutations = Array<Mutation>;
+
+const SIZE = 15;
+const MAX = Math.floor(SIZE / 2);
+const MIN = -1 * MAX;
+const SCALE = 48;
+const RADIUS = 18;
+const STROKE = 36;
+const SYMMETRY = true;
+const DEBUG = true;
+const THICKNESS = 2;
+const DEPTH = STROKE / 2;
+const PAGES = DEPTH / THICKNESS;
+
+const LEFT: Point = [-1, 0];
+const RIGHT: Point = [1, 0];
+const UP: Point = [0, -1];
+const DOWN: Point = [0, 1];
 
 const DIR = [UP, DOWN, LEFT, RIGHT];
 const Opposite = [
@@ -54,7 +76,7 @@ const PRINT = ['↑', '↓', '←', '→'];
 const X = 0;
 const Y = 1;
 
-const start: Point = [Math.floor(SIZE / 2) - 1, Math.floor(SIZE / 2) + 1];
+const START: Point = [Math.floor(SIZE / 2) - 1, Math.floor(SIZE / 2) + 1];
 const initial: Path = [
   Direction.Up,
   Direction.Up,
@@ -66,7 +88,11 @@ const initial: Path = [
   Direction.Left
 ];
 
-function makeBoard(size, path: Path, start) {
+function makeBoard(
+  path: Path,
+  start: Point = START,
+  size: number = SIZE
+): Board {
   let board = [];
   let length = size * size;
   for (let i = 0; i < length; i++) {
@@ -74,19 +100,30 @@ function makeBoard(size, path: Path, start) {
   }
   let x = start[X];
   let y = start[Y];
-  return path.reduce(function(board, current, index, array) {
-    board[y * size + x] = current;
-    x += DIR[current][X];
-    y += DIR[current][Y];
+  return path.reduce(function(board, direction, index, path) {
+    const next = (index + 1) % path.length;
+    board[y * size + x] = {direction, point: [x, y], index, next};
+    x += DIR[direction][X];
+    y += DIR[direction][Y];
     return board;
   }, board);
 }
 
-function boardToString(board, size) {
+function getBoardSpace(
+  board: Board,
+  x: number,
+  y: number,
+  size: number = SIZE
+): BoardSpace {
+  const index = y * size + x;
+  return board[index];
+}
+
+function boardToString(board, size: number = SIZE) {
   return board.reduce(function(output, current, index, board) {
     let char = '. ';
     if (current != null) {
-      char = PRINT[current] + ' ';
+      char = PRINT[current.direction] + ' ';
     }
     if (index > 0 && (index + 1) % SIZE === 0) {
       char += '\n';
@@ -95,67 +132,163 @@ function boardToString(board, size) {
   }, '');
 }
 
-const initialBoard = makeBoard(SIZE, initial, start);
-console.log(initialBoard);
-console.log(boardToString(initialBoard, SIZE));
+// const mutations: Mutations = [
+//   [0, Direction.Left],
+//   [1, Direction.Left],
+//   [7, Direction.Up],
+//   [8, Direction.Up]
+// ];
 
-// function Make
-
-type Mutation = [number, Direction];
-type Mutations = Array<Mutation>;
-
-const mutations: Mutations = [
-  [0, Direction.Left],
-  [1, Direction.Left],
-  [7, Direction.Up],
-  [8, Direction.Up]
-];
-
-function makePath(directions: Path, mutations: Mutations) {
-  const directionsBumped: Path = mutations.reduce(function(
-    last,
-    current,
-    mutationIndex,
-    array
+function pathWithMutations(path: Path, mutations: Mutations): Path {
+  return mutations.reduce(function(
+    last: Path,
+    current: Mutation,
+    mutationIndex: number,
+    array: Mutations
   ) {
     const [index, direction] = current;
     last.splice(index, 0, direction);
     const opposite = Opposite[direction];
-    last.splice((index + 2) % last.length, 0, opposite);
-    if (symmetry) {
+    const index2 = (index + 2) % (last.length + 1);
+    last.splice(index2, 0, opposite);
+    if (SYMMETRY) {
+      // TODO wrap bugs
       const len = last.length;
-      const symIndex = (index + Math.floor(len / 2) + 1) % (len + 1);
-      console.log(symIndex);
+      let symIndex = (index + len / 2 + 1) % len;
+      if (symIndex === 0) {
+        symIndex = len;
+      }
       last.splice(symIndex, 0, opposite);
-      const symIndex2 = (symIndex + 2) % (last.length + 1);
-      console.log(symIndex2);
+      let symIndex2 = (symIndex + 2) % last.length;
+      if (symIndex2 === 0) {
+        symIndex2 = last.length;
+      }
       last.splice(symIndex2, 0, direction);
     }
-    if (DEBUG) {
-      // console.log(last);
-      console.log(last.map(i => PRINT[i]).join(' '));
-      console.log(boardToString(makeBoard(SIZE, last, start), SIZE));
-    }
     return last;
-  }, directions.slice());
+  }, path.slice());
+}
 
+function makeAbsPath(path: Path, start: Point = START): AbsPath {
   let x = start[X];
   let y = start[Y];
 
-  const path = directionsBumped.map(function(direction) {
+  return path.map(function(direction) {
     const absolutePoint: Point = [x, y];
     x += DIR[direction][X];
     y += DIR[direction][Y];
     return absolutePoint;
   });
-
-  return path;
 }
 
-const laby = makePath(initial, mutations);
-console.log(laby);
+function getPossibleMutations(path: Path, size: number = SIZE): Mutations {
+  const board = makeBoard(path);
+  return board.reduce(function(
+    possible: Mutations,
+    boardSpace: BoardSpace,
+    boardIndex,
+    board
+  ) {
+    if (!boardSpace) return possible;
+    const {direction, point, index, next} = boardSpace;
+    const nextDirection = path[next];
+    const [x, y] = point;
+    switch (direction) {
+      case Direction.Up:
+        if (
+          (nextDirection === Direction.Up ||
+            nextDirection === Direction.Right) &&
+          x > 0 &&
+          y > 0
+        ) {
+          const bump1 = getBoardSpace(board, x - 1, y);
+          const bump2 = getBoardSpace(board, x - 1, y - 1);
+          if (!bump1 && !bump2) {
+            possible.push([index, Direction.Left]);
+          }
+        }
+        break;
+      case Direction.Down:
+        if (
+          (nextDirection === Direction.Down ||
+            nextDirection === Direction.Left) &&
+          x < size - 1 &&
+          y < size - 1
+        ) {
+          const bump1 = getBoardSpace(board, x + 1, y);
+          const bump2 = getBoardSpace(board, x + 1, y + 1);
+          if (!bump1 && !bump2) {
+            possible.push([index, Direction.Right]);
+          }
+        }
+        break;
+      case Direction.Left:
+        if (
+          (nextDirection === Direction.Left ||
+            nextDirection === Direction.Up) &&
+          x > 0 &&
+          y < size - 1
+        ) {
+          const bump1 = getBoardSpace(board, x, y + 1);
+          const bump2 = getBoardSpace(board, x - 1, y + 1);
+          if (!bump1 && !bump2) {
+            possible.push([index, Direction.Down]);
+          }
+        }
+        break;
+      case Direction.Right:
+        if (
+          (nextDirection === Direction.Right ||
+            nextDirection === Direction.Down) &&
+          x < size - 1 &&
+          y > 0
+        ) {
+          const bump1 = getBoardSpace(board, x, y - 1);
+          const bump2 = getBoardSpace(board, x + 1, y - 1);
+          if (!bump1 && !bump2) {
+            possible.push([index, Direction.Up]);
+          }
+        }
+        break;
+      default:
+        break;
+    }
+    return possible;
+  }, []);
+}
 
-function pathToMaker(path): MakerJs.IModel {
+function logPath(path) {
+  console.log(path.map(i => PRINT[i]).join(' '));
+  console.log(boardToString(makeBoard(path)));
+}
+
+function randomMutations(path: Path): Mutations {
+  const mutations = [];
+  if (DEBUG) {
+    logPath(path);
+  }
+  let possible = getPossibleMutations(path);
+  while (possible.length > 0) {
+    const mutation = possible[Math.floor(Math.random() * possible.length)];
+    mutations.push(mutation);
+    const currentPath = pathWithMutations(path, mutations);
+    if (DEBUG) {
+      logPath(currentPath);
+    }
+    possible = getPossibleMutations(currentPath);
+    if (mutations.length > SIZE * SIZE) {
+      console.error('oops');
+      return;
+    }
+  }
+  return mutations;
+}
+
+const mutations = randomMutations(initial);
+const laby = makeAbsPath(pathWithMutations(initial, mutations));
+// console.log(laby);
+
+function pathToMaker(path: AbsPath): MakerJs.IModel {
   const length = path.length;
 
   const lines = path.reduce(function(last, current, index, array) {
@@ -165,36 +298,85 @@ function pathToMaker(path): MakerJs.IModel {
     return last;
   }, {});
 
-  const fillets = path.reduce(function(last, current, index, array) {
-    const fillet = MakerJs.path.fillet(
-      lines['line' + index],
-      lines['line' + (index + 1) % length],
-      RADIUS
-    );
-    if (fillet) {
-      last['fillet' + index] = fillet;
-    } else {
-      console.warn("Couldn't fillet", index);
-    }
-    return last;
-  }, {});
+  const output: MakerJs.IModel = {paths: lines};
 
-  const paths = {...lines, ...fillets};
+  MakerJs.model.findChains(output, function(chains, loose, layer) {
+    const chain = chains[0];
+    const fillets = MakerJs.chain.fillet(chain, RADIUS);
+    output.models = {fillets};
+  });
+
   // Mirror Y to normalize coordinate system
-  const mirrored = MakerJs.model.mirror({paths}, false, true);
+  const mirrored = MakerJs.model.mirror(output, false, true);
   return mirrored;
 }
 
 const makerPath = pathToMaker(laby);
-const expanded = MakerJs.model.expandPaths(makerPath, STROKE);
+
+function simpleExpand(model, stroke = STROKE): MakerJs.IModel {
+  const paths = {};
+  const lines = model.paths;
+  const stroke2 = stroke / 2;
+  for (let key in lines) {
+    if (lines.hasOwnProperty(key)) {
+      let line: MakerJs.paths.Line = lines[key];
+      if (line.end[X] === line.origin[X]) {
+        paths[key + 'L'] = MakerJs.path.moveRelative(
+          MakerJs.cloneObject(line),
+          [-stroke2, 0]
+        );
+        paths[key + 'R'] = MakerJs.path.moveRelative(
+          MakerJs.cloneObject(line),
+          [stroke2, 0]
+        );
+      } else {
+        paths[key + 'U'] = MakerJs.path.moveRelative(
+          MakerJs.cloneObject(line),
+          [0, stroke2]
+        );
+        paths[key + 'D'] = MakerJs.path.moveRelative(
+          MakerJs.cloneObject(line),
+          [0, -stroke2]
+        );
+      }
+    }
+  }
+  const fillets = model.models.fillets.paths;
+  for (let key in fillets) {
+    if (fillets.hasOwnProperty(key)) {
+      let fillet = fillets[key];
+      paths[key + 'In'] = {...fillet, radius: fillet.radius - stroke2};
+      paths[key + 'Out'] = {...fillet, radius: fillet.radius + stroke2};
+    }
+  }
+  return {paths};
+}
+
+// const expanded = MakerJs.model.expandPaths(makerPath, STROKE);
+
+// For stacked paper with approx circular path
+const expanded = {}
+for (let i = 0; i < PAGES; i++) {
+  const depth = THICKNESS * i;
+  // Length of chord, like Pythagoras
+  const stroke = 2 * Math.sqrt(DEPTH * DEPTH - depth * depth);
+  console.log(depth, stroke)
+  expanded['expanded' + i] = simpleExpand(makerPath, stroke);
+}
 
 const cut = {
-  paths: makerPath.paths,
   models: {
-    expanded
+    // line: makerPath,
+    expanded: {models: expanded}
   }
 };
 
 console.log(cut);
-const svg = MakerJs.exporter.toSVG(cut);
+const svg = MakerJs.exporter.toSVG(
+  cut,
+  {
+    // useSvgPathOnly: !DEBUG,
+    // annotate: DEBUG
+  }
+);
 document.write(svg);
