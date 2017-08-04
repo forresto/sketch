5eed738c68fc5a333c16791f3bf7ecc3988e94a0
Forrest Oliphant <forrest@sembiki.com>
2017-08-04 10:38:14 +0300

    fancy console debugging, match coord sys

--- labyrinth-03.maker.ts
+++ labyrinth-03.maker.ts
@@ -1,47 +1,117 @@
-/// <reference path="node_modules/@types/maker.js/index.d.ts" />
+/// <reference path="node_modules/makerjs/index.d.ts" />
+
+import * as MakerJs from 'makerjs';
 
 /*
 
-* * * R-D * *
-* * * U D * *
-* * R-U R-R-D
-R-R-U * D-L-L
-U-L-L D-D * *
-* * U D * * *
-* * U-L * * *
+← ← ↑ → → ↑ → ↑ ↑ → ↓ ↓ → → ↓ ← ← ↓ ← ↓ ↓ ← ↑ ↑
+
+. . . → ↓ . . 
+. . . ↑ ↓ . . 
+. . → ↑ → → ↓ 
+→ → ↑ . ↓ ← ← 
+↑ ← ← ↓ ← . . 
+. . ↑ ↓ . . . 
+. . ↑ ← . . . 
 
 */
 
-
-const makerjs = require('makerjs');
-
-const SIZE = 10;
+const SIZE = 7;
 const MAX = Math.floor(SIZE / 2);
 const MIN = -1 * MAX;
 const SCALE = 72;
 const RADIUS = 24;
 const STROKE = 12;
 const symmetry = true;
+const DEBUG = true;
 
 type Point = [number, number];
-type Path = Array<Point>;
-type Model = {paths?: any; models?: any};
 
-const L: Point = [-1, 0];
-const R: Point = [1, 0];
-const U: Point = [0, 1];
-const D: Point = [0, -1];
+const LEFT: Point = [-1, 0];
+const RIGHT: Point = [1, 0];
+const UP: Point = [0, -1];
+const DOWN: Point = [0, 1];
 
-const start: Point = [Math.floor(SIZE / 2), Math.floor(SIZE / 2)];
+const enum Direction {
+  Up,
+  Down,
+  Left,
+  Right
+}
 
-const initial: Path = [U, U, R, R, D, D, L, L];
+type Path = Array<Direction>;
 
-type Mutation = [number, Point];
+const DIR = [UP, DOWN, LEFT, RIGHT];
+const Opposite = [
+  Direction.Down,
+  Direction.Up,
+  Direction.Right,
+  Direction.Left
+];
+// const OPP = [DOWN, UP, RIGHT, LEFT];
+const PRINT = ['↑', '↓', '←', '→'];
+
+const X = 0;
+const Y = 1;
+
+const start: Point = [Math.floor(SIZE / 2) - 1, Math.floor(SIZE / 2) + 1];
+const initial: Path = [
+  Direction.Up,
+  Direction.Up,
+  Direction.Right,
+  Direction.Right,
+  Direction.Down,
+  Direction.Down,
+  Direction.Left,
+  Direction.Left
+];
+
+function makeBoard(size, path: Path, start) {
+  let board = [];
+  let length = size * size;
+  for (let i = 0; i < length; i++) {
+    board[i] = null;
+  }
+  let x = start[X];
+  let y = start[Y];
+  return path.reduce(function(board, current, index, array) {
+    board[y * size + x] = current;
+    x += DIR[current][X];
+    y += DIR[current][Y];
+    return board;
+  }, board);
+}
+
+function boardToString(board, size) {
+  return board.reduce(function(output, current, index, board) {
+    let char = '. ';
+    if (current != null) {
+      char = PRINT[current] + ' ';
+    }
+    if (index > 0 && (index + 1) % SIZE === 0) {
+      char += '\n';
+    }
+    return output + char;
+  }, '');
+}
+
+const initialBoard = makeBoard(SIZE, initial, start);
+console.log(initialBoard);
+console.log(boardToString(initialBoard, SIZE));
+
+// function Make
+
+type Mutation = [number, Direction];
 type Mutations = Array<Mutation>;
 
-const mutations: Mutations = [[0, L], [1, L], [7, U], [9, U]];
+const mutations: Mutations = [
+  [0, Direction.Left],
+  [1, Direction.Left],
+  [7, Direction.Up],
+  [8, Direction.Up]
+];
 
-function makePath(directions: Path, mutations: Mutations): Path {
+function makePath(directions: Path, mutations: Mutations) {
   const directionsBumped: Path = mutations.reduce(function(
     last,
     current,
@@ -50,24 +120,32 @@ function makePath(directions: Path, mutations: Mutations): Path {
   ) {
     const [index, direction] = current;
     last.splice(index, 0, direction);
-    const opposite: Point = [0 - direction[0], 0 - direction[1]];
+    const opposite = Opposite[direction];
     last.splice((index + 2) % last.length, 0, opposite);
     if (symmetry) {
       const len = last.length;
-      const index2 = (index + 1 + Math.floor(len / 2)) % len;
-      last.splice(index2, 0, opposite);
-      last.splice((index2 + 2) % last.length, 0, direction);
+      const symIndex = (index + Math.floor(len / 2) + 1) % (len + 1);
+      console.log(symIndex);
+      last.splice(symIndex, 0, opposite);
+      const symIndex2 = (symIndex + 2) % (last.length + 1);
+      console.log(symIndex2);
+      last.splice(symIndex2, 0, direction);
+    }
+    if (DEBUG) {
+      // console.log(last);
+      console.log(last.map(i => PRINT[i]).join(' '));
+      console.log(boardToString(makeBoard(SIZE, last, start), SIZE));
     }
     return last;
   }, directions.slice());
 
-  let x = start[0];
-  let y = start[1];
+  let x = start[X];
+  let y = start[Y];
 
-  const path: Path = directionsBumped.map(function(direction) {
-    x += direction[0];
-    y += direction[1];
+  const path = directionsBumped.map(function(direction) {
     const absolutePoint: Point = [x, y];
+    x += DIR[direction][X];
+    y += DIR[direction][Y];
     return absolutePoint;
   });
 
@@ -75,19 +153,20 @@ function makePath(directions: Path, mutations: Mutations): Path {
 }
 
 const laby = makePath(initial, mutations);
+console.log(laby);
 
-function pathToMaker(path: Path): Model {
+function pathToMaker(path): MakerJs.IModel {
   const length = path.length;
 
   const lines = path.reduce(function(last, current, index, array) {
-    const line = new makerjs.paths.Line(current, array[(index + 1) % length]);
-    const scaled = makerjs.path.scale(line, SCALE);
+    const line = new MakerJs.paths.Line(current, array[(index + 1) % length]);
+    const scaled = MakerJs.path.scale(line, SCALE);
     last['line' + index] = scaled;
     return last;
   }, {});
 
   const fillets = path.reduce(function(last, current, index, array) {
-    const fillet = makerjs.path.fillet(
+    const fillet = MakerJs.path.fillet(
       lines['line' + index],
       lines['line' + (index + 1) % length],
       RADIUS
@@ -101,11 +180,13 @@ function pathToMaker(path: Path): Model {
   }, {});
 
   const paths = {...lines, ...fillets};
-  return {paths};
+  // Mirror Y to normalize coordinate system
+  const mirrored = MakerJs.model.mirror({paths}, false, true);
+  return mirrored;
 }
 
 const makerPath = pathToMaker(laby);
-const expanded = makerjs.model.expandPaths(makerPath, STROKE);
+const expanded = MakerJs.model.expandPaths(makerPath, STROKE);
 
 const cut = {
   paths: makerPath.paths,
@@ -115,5 +196,5 @@ const cut = {
 };
 
 console.log(cut);
-const svg = makerjs.exporter.toSVG(cut);
+const svg = MakerJs.exporter.toSVG(cut);
 document.write(svg);
